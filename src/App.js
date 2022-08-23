26c0ab8c8a158ea2c2419084adfc2c5b98825c0d
sabit1997 <yejiii1997@gmail.com>
2022-08-23 22:38:27 +0900

    Feat: GlobalStyle, Reset 적용 및 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
-
+import GlobalStyle from './styles/GlobalStyle';
+import { Reset } from 'styled-reset';
+import Home from './page/Home';
 function App() {
   return (
-    <Routes>
-      <Route path="/" />
-      <Route path="/login" />
-      <Route path="/join" />
-      <Route path="/cart" />
-      <Route path="product" />
-      <Route path="payment" />
-      <Route path="notfound" />
-    </Routes>
+    <div>
+      <GlobalStyle />
+      <Reset />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" />
+        <Route path="/join" />
+        <Route path="/cart" />
+        <Route path="product" />
+        <Route path="payment" />
+        <Route path="notfound" />
+      </Routes>
+    </div>
   );
 }
 
