1bf15e44ed18f014c4a4dd9d3aed0e82f80b617d
sabit1997 <yejiii1997@gmail.com>
2022-09-06 21:49:04 +0900

    Feat: cart 라우터 연결

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import { Reset } from 'styled-reset';
 import Home from './page/Home';
 import Login from './page/Login';
 import Join from './page/Join';
+import Cart from './page/Cart';
+
 function App() {
   return (
-    <div>
+    <>
       <GlobalStyle />
       <Reset />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
-        <Route path="/cart" />
+        <Route path="/cart" element={<Cart />} />
         <Route path="product" />
         <Route path="payment" />
         <Route path="notfound" />
       </Routes>
-    </div>
+    </>
   );
 }
 
