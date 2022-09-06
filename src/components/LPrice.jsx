25e672e01a9dd444cce05be400ce34b4b5f95afe
sabit1997 <yejiii1997@gmail.com>
2022-09-06 19:57:33 +0900

    Feat: LPrice 컴포넌트 추가

---
+++ src/components/LPrice.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export default function LPrice(props) {
+  return (
+    <Price marginB={props.marginB} cl={props.cl}>
+      {props.value}
+    </Price>
+  );
+}
+
+const Price = styled.p`
+  font-size: 36px;
+  font-weight: 700;
+  line-height: 1.251944444444444;
+  color: ${(props) => props.cl};
+  margin-bottom: ${(props) => props.marginB};
+  &::after {
+    content: '원';
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 1.252222222222222;
+    margin-left: 2px;
+  }
+`;
