4471c624255dfbc6aa4af2204ac7740dff2585e6
sabit1997 <yejiii1997@gmail.com>
2022-09-08 14:43:36 +0900

    Feat: Payment 페이지 라우터 연결

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './page/Home';
 import Login from './page/Login';
 import Join from './page/Join';
 import Cart from './page/Cart';
+import Payment from './page/Payment';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
         <Route path="product" />
         <Route path="payment" />
         <Route path="notfound" />
