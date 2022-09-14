abf8d17441a24e566401e65f6d00c890038d4601
sabit1997 <yejiii1997@gmail.com>
2022-09-14 15:39:25 +0900

    Feat: Warpper를 form 으로 변경

--- src/components/join/JoinStyle.jsx
+++ src/components/join/JoinStyle.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Warpper = styled.section`
+export const Warpper = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
