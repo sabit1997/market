69a4c6cfbb3321958ca7c3a6a1a2cee06fba71b4
sabit1997 <yejiii1997@gmail.com>
2022-10-18 22:56:14 +0900

    Fix: 로그인 하지 않았을 때 TopNavBar 나타내기

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,11 +1,36 @@
-import BuyerHome from './BuyerHome';
-import SellerHome from './SellerHome';
+import TopNavBar from '../../components/navBar/TopNavBar';
+// import CarouselImg1 from '../assets/carouselImgs/CarouselImg1.png';
+// import CarouselImg2 from '../assets/carouselImgs/CarouselImg2.png';
+// import CarouselImg3 from '../assets/carouselImgs/CarouselImg3.png';
+import ProductList from '../../components/contents/ProductList';
+import { PageWarpper } from '../../components/common/Common';
+import { ProductListSection } from '../../components/home/HomeStyle';
+import Footer from '../../components/footer/Footer';
+import Carousel from '../../components/carousel/Carousel';
+import { useProductDataContext } from '../../context/ProductDataContext';
 
-export default function Home() {
+export default function BuyerHome() {
+  const { productData } = useProductDataContext();
   const loginType = localStorage.getItem('type');
-  if (loginType === 'SELLER') {
-    return <SellerHome />;
-  } else if (loginType === 'BUYER') {
-    return <BuyerHome />;
-  }
+
+  const productList = productData.map((_, i) => (
+    <ProductList
+      productData={productData}
+      i={i}
+      key={productData[i].product_id}
+    />
+  ));
+
+  return (
+    <PageWarpper>
+      {loginType === 'BUYER' || loginType === null ? (
+        <TopNavBar />
+      ) : loginType === 'SELLER' ? (
+        <TopNavBar value="seller" />
+      ) : null}
+      <Carousel />
+      <ProductListSection>{productList}</ProductListSection>
+      <Footer />
+    </PageWarpper>
+  );
 }
