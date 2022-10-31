68fd01e3820d2f96063276ff0b4ab6e38080bd1a
sabit1997 <yejiii1997@gmail.com>
2022-10-31 21:29:13 +0900

    Fix: 수량에 따라 총 상품 금액 변경

--- src/page/productDetail/ProductDetail.jsx
+++ src/page/productDetail/ProductDetail.jsx
@@ -21,11 +21,15 @@ import MDarkButton from '../../components/button/MDarkButton';
 import TabActiveButton from '../../components/button/TabActiveButton';
 import TabDisabledButton from '../../components/button/TabDisabledButton';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
+import instance from '../../client/instance';
 
 export default function ProductDetail() {
   const { product_id } = useParams();
   const [productDetail, setProductDetail] = useState('');
+  const [amountQuantity, setAmountQuantity] = useState(1);
+  const location = useLocation();
+
   useEffect(() => {
     client
       .get(`/products/${product_id}/`)
@@ -38,7 +42,43 @@ export default function ProductDetail() {
       });
   }, [product_id]);
 
+  function shippingValue(fee, method) {
+    if (method === 'PARCEL') {
+      if (fee === 0) {
+        return '소포배송 / 무료배송';
+      } else if (fee !== 0) {
+        return `소포배송 / ${fee?.toLocaleString()}원`;
+      }
+    } else if (method === 'DELIVERY') {
+      if (fee === 0) {
+        return '택배배송 / 무료배송';
+      } else if (fee !== 0) {
+        return `택배배송 / ${fee?.toLocaleString()}원`;
+      }
+    }
+  }
+
+  // 장바구니에 넣기
+
+  function handleButton() {
+    instance
+      .post('/cart/', {
+        product_id: product_id,
+        quantity: amountQuantity,
+        check: true,
+        // check 채우는건 cart 불러오고 나서 할 수 있을듯
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   console.log(productDetail.stock);
+  console.log(product_id);
+  console.log(location.pathname);
   return (
     <>
       <TopNavBar />
@@ -49,11 +89,23 @@ export default function ProductDetail() {
           <ProductName>{productDetail.product_name}</ProductName>
           <LPrice
             cl="#000"
-            value={productDetail.price}
+            value={productDetail.price?.toLocaleString()}
             marginB="138px"
           ></LPrice>
-          <Shipping>{`${productDetail.shipping_method} / ${productDetail.shipping_fee}`}</Shipping>
-          <Amount marginT="52px" stock={productDetail.stock} />
+          <Shipping>
+            {shippingValue(
+              productDetail.shipping_fee,
+              productDetail.shipping_method
+            )}
+          </Shipping>
+          <Amount
+            margin="52px 0 0"
+            stock={productDetail.stock}
+            setAmountQuantity={setAmountQuantity}
+            amountQuantity={amountQuantity}
+            value={amountQuantity}
+            product_id={product_id}
+          />
           <PriceWarpper>
             <TotalPriceTxt>총 상품 금액</TotalPriceTxt>
             <RightWarpper>
@@ -61,15 +113,20 @@ export default function ProductDetail() {
                 총 수량{' '}
                 <TotalQuantityNum>{productDetail.stock}</TotalQuantityNum>개
               </TotalQuantity>
-              <LPrice cl="#21BF48" value={productDetail.price}></LPrice>
+              <LPrice
+                cl="#21BF48"
+                value={(productDetail.price * amountQuantity)?.toLocaleString()}
+              ></LPrice>
             </RightWarpper>
           </PriceWarpper>
           <MButton wd="416px" value="바로 구매" marginR="14px" />
           <MDarkButton
             wd="200px"
-            value="장바구니"
             product_id={product_id}
             productDetail={productDetail}
+            amountQuantity={amountQuantity}
+            value="장바구니"
+            onClick={handleButton}
           />
         </ProductInfo>
       </ProductWarpper>
