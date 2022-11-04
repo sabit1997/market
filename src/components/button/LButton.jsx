da410bd863ff29a608adf9e241d61abe4e20a240
sabit1997 <yejiii1997@gmail.com>
2022-11-04 21:08:10 +0900

    Fix: onClick 이벤트 받아오기

--- src/components/button/LButton.jsx
+++ src/components/button/LButton.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 export default function LButton(props) {
   return (
     <>
-      <Button margin={props.margin}>{props.value}</Button>
+      <Button margin={props.margin} onClick={props.onClick}>
+        {props.value}
+      </Button>
     </>
   );
 }
