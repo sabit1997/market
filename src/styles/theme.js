99f16f494783a5e68cb81d06a95ae391caee7978
sabit1997 <yejiii1997@gmail.com>
2022-11-30 14:43:41 +0900

    Feat: theme 추가

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const size = {
+  mobile: '390px',
+  tablet: '834px',
+  desktop: '1440px',
+};
+
+const theme = {
+  mainColor: '#21BF48',
+  mobile: `screen and (max-width: ${size.mobile})`,
+  tablet: `screen and (max-width: ${size.tablet})`,
+  desktop: `screen and (max-width: ${size.desktop})`,
+};
+
+export default theme;
